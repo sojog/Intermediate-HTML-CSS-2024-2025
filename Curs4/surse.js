98920ac4dfcc7825007f0cb7a381712b4ad46baa
Silviu Ojog <sil.ojog@gmail.com>
2025-05-27 20:50:39 +0300

    Curs4

---
+++ Curs4/surse.js
@@ -0,0 +1,51 @@
+
+bookings = [
+    {
+        "image": "https://a0.muscache.com/im/pictures/hosting/Hosting-1142706735503645966/original/5a18425b-43f5-4ac6-b2bc-588bc906b44f.jpeg?im_w=720",
+        "location": "Brașov, România",
+        "rating": 4.8,
+        "distance": "2.5 km",
+        "price": "450 lei",
+        "period": "Jun 1-13"
+    },
+    {
+        "image": "https://a0.muscache.com/im/pictures/miso/Hosting-37805118/original/58212748-d05c-46c4-9c93-5194e4c6bfb4.jpeg?im_w=720",
+        "location": "Sinaia, România",
+        "rating": 4.5,
+        "distance": "1.2 km",
+        "price": "380 lei",
+        "period": "Jun 18-23"
+    },
+    {
+        "image": "https://a0.muscache.com/im/pictures/ba79df4c-7180-4baf-a90d-8acb78de4b0a.jpg?im_w=720",
+        "location": "Cluj-Napoca, România",
+        "rating": 4.9,
+        "distance": "3.8 km",
+        "price": "520 lei",
+        "period": "Jan 8-13"
+    },
+    {
+        "image": "https://a0.muscache.com/im/pictures/ad45d37e-d673-4e36-ba0b-c1bf9d171285.jpg?im_w=720",
+        "location": "Constanța, România",
+        "rating": 4.7,
+        "distance": "0.9 km",
+        "price": "600 lei",
+        "period": "Dec 8-13"
+    },
+    {
+        "image": "https://a0.muscache.com/im/pictures/miso/Hosting-52860822/original/fd35d7f3-a6c8-4a99-9218-8277bacf8347.png?im_w=720",
+        "location": "Maramureș, România",
+        "rating": 4.6,
+        "distance": "4.1 km",
+        "price": "340 lei",
+        "period": "Jul 2-13"
+    },
+    {
+        "image": "https://a0.muscache.com/im/pictures/3fb32733-3f57-46d4-a058-8da195405c4e.jpg?im_w=720",
+        "location": "Râșnov, România",
+        "rating": 4.3,
+        "distance": "2.7 km",
+        "price": "410 lei",
+        "period": "Jun 5-13"
+    }
+]
